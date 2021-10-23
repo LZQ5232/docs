ea88a5e471bb25978e1e2e2c9d1e671ce0cbbe7a
linziqian <2909649206@qq.com>
2021-10-23 19:19:20 +0800

    feat:travis ci

---
+++ cname.sh
@@ -0,0 +1,7 @@
+#cname.sh
+
+#!/usr/bin/env sh
+
+set -e
+
+echo 'https://lzq5232.github.io/docs/' > docs/.vuepress/dist/CNAME
--- docs/about.md
+++ docs/about.md
@@ -8,3 +8,5 @@ tags:
 pageClass: custom-page-class
 ---
 # about
+
+添加travis ci
