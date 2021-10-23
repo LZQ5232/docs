f0ddfd89a0f14997581cf27583f1ba8e152c64d1
linziqian <2909649206@qq.com>
2021-10-23 18:30:52 +0800

    feat:vssuw

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/docs/',
   title: 'Hello VuePress',
   description: 'Just playing around',
   themeConfig: {
@@ -33,29 +34,57 @@ module.exports = {
         initialOpenGroupIndex: -1 // 可选的, 默认值是 0
       }
     ],
-    displayAllHeaders: true,
+    // displayAllHeaders: true,
     // search: true, // 展示搜索框
     // searchMaxSuggestions: 10, // 搜索框显示的搜索结果数量
     // nextLinks: false, 禁用所有页面的下一篇链接
     // prevLinks: false, 禁用所有页面的上一篇 链接
 
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'vuejs/vuepress',
+    // repo: 'vuejs/vuepress',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
+    // repoLabel: '查看源码',
 
     // 以下为可选的编辑链接选项
 
     // 假如你的文档仓库和项目本身不在一个仓库：
-    docsRepo: 'vuejs/vuepress',
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！'
+    // docsRepo: 'vuejs/vuepress',
+    // // 假如文档不是放在仓库的根目录下：
+    // docsDir: 'docs',
+    // // 假如文档放在一个特定的分支下：
+    // docsBranch: 'master',
+    // // 默认是 false, 设置为 true 来启用
+    // editLinks: true,
+    // // 默认为 "Edit this page"
+    // editLinkText: '帮助我们改善此页面！'
+  },
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  plugins: {
+    '@vssue/vuepress-plugin-vssue': {
+      // 设置 `platform` 而不是 `api`
+      platform: 'github-v4',
+
+      // 其他的 Vssue 配置
+      owner: 'LZQ5232',
+      repo: 'docs',
+      clientId: '043c7d96645e29b62da7',
+      clientSecret: '579c8757dfc6ff07f89d5da996eaf685e13b5345',
+      autoCreateIssue:true
+    },
+    // '@vuepress/pwa': {
+    //   serviceWorker: true,
+    //   updatePopup: true
+    // }
   }
 }
